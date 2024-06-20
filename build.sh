0793765eaad2bb56d727abb89a9dd0c619690b50
Cesar1928 <cesarbrianlozada@gmail.com>
2024-06-20 17:46:37 -0500

    T commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Apply any outstanding database migrations
+python manage.py migrate
